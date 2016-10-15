7f7b5f9857ea75b0ac40f5eb58e86fedcdff6785
DataNick <DataNick@users.noreply.github.com>
2016-10-15 19:16:23 -0400

    Create update price method to set most recent price to new movement.

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,3 +1,10 @@
 class Movement < ApplicationRecord
   belongs_to :position
+
+  def update_price
+    # binding.pry
+    ticker = self.position['ticker']
+    price = CLIENT.quotes([ticker], [:last_trade_price]).first
+    self.update_attribute( :price, price.last_trade_price )
+  end
 end

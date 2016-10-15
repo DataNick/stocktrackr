3f5ee113f9c187a37a492b20bad8896b9e8a1198
DataNick <DataNick@users.noreply.github.com>
2016-10-15 16:28:01 -0400

    Add validations before saving object.

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,6 +1,26 @@
 class Position < ApplicationRecord
   belongs_to :portfolio
-  has_many :movements
+  has_many :movements, dependent: :destroy
 
   validates_uniqueness_of :ticker, scope: :portfolio_id
+  validates_presence_of :name,
+                        :message => "Stock not found on Yahoo Finance."
+  before_validation :get_quote_name
+
+  def buy(quantity)
+    ticker.upcase!
+    movements.build(date: DateTime.now, quantity: quantity, price: quote.last_trade_price, trade: 'buy')
+    save
+  end
+
+  private
+
+  def quote
+    @quote ||= CLIENT.quotes([ticker], [:last_trade_price, :last_trade_date, :name, :symbol]).first
+  end
+
+  def get_quote_name
+    self.name = @quote.name
+  end
+
 end

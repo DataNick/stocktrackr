1e756fe0374f44dd96cdd4f24d5ec8654c2a4c4b
DataNick <DataNick@users.noreply.github.com>
2016-10-15 16:26:22 -0400

    Modify create action and add quote and update_name methods

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -2,6 +2,17 @@ class PositionsController < ApplicationController
   before_action :authenticate_user!
   before_action :load_portfolio
   def create
+    @position = @portfolio.positions.build(position_params)
+    respond_to do |format|
+      if @position.buy(params[:position][:quantity].to_i)
+        format.html { redirect_to @portfolio, notice: 'Position was successfully created.' }
+        format.json { render :show, status: :created, location: @position }
+      else
+        format.html { render 'portfolios/show' }
+        format.json { render json: @position.errors, status: :unprocessable_entity }
+        format.js
+      end
+    end
   end
 
   def new
@@ -19,5 +30,9 @@ class PositionsController < ApplicationController
 
     def load_portfolio
       @portfolio = Portfolio.find(params[:portfolio_id])
+    end
+
+    def position_params
+      params.require(:position).permit(:ticker, :name, :quantity, :portfolio_id)
     end
 end

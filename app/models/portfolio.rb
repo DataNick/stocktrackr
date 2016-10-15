a86e845f58565b3301618d7b03edfac4b7921d55
DataNick <DataNick@users.noreply.github.com>
2016-10-14 22:37:35 -0400

    Correct validation error

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,5 +3,4 @@ class Portfolio < ApplicationRecord
   has_many :movements, through: :positions
   belongs_to :user
 
-  validates_uniqueness_of :ticker, scope: :portfolio_id
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,4 +1,6 @@
 class Position < ApplicationRecord
   belongs_to :portfolio
   has_many :movements
+
+  validates_uniqueness_of :ticker, scope: :portfolio_id
 end

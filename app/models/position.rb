eaa5109a3a43d5d0600331ed1364a4f0438825e0
DataNick <DataNick@users.noreply.github.com>
2016-10-22 14:52:57 -0400

    Set price for position

--- app/models/position.rb
+++ app/models/position.rb
@@ -7,7 +7,8 @@ class Position < ApplicationRecord
   validates_presence_of :quantity, :message => 'Must input a number.'
   validates_presence_of :name, :message => "Search for name revealed no match."
 
-  before_validation :search_name
+  before_validation :set_name
+  before_validation :set_price
   before_create :upcase_ticker
 
   def buy(quantity)
@@ -34,8 +35,12 @@ class Position < ApplicationRecord
     YAHOO_CLIENT.quotes([ticker], [:last_trade_price, :last_trade_date, :name, :symbol]).first
   end
 
-  def search_name
-    self.name = get_quote['name'] if get_quote['name'] != "N/A"
+  def set_name
+    self.name = get_quote.name if get_quote['name'] != "N/A"
+  end
+
+  def set_price
+    self.price = get_quote.last_trade_price if get_quote['name'] != 'N/A'
   end
 
   def upcase_ticker

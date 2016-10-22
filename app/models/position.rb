b25dbfb64f5fdf3251533912b2d87508b91d86bd
DataNick <DataNick@users.noreply.github.com>
2016-10-22 03:53:52 -0400

    Ensure validations work and users cannot submit empty or invalid forms.

--- app/models/position.rb
+++ app/models/position.rb
@@ -3,29 +3,37 @@ class Position < ApplicationRecord
   has_many :movements, dependent: :destroy
 
   validates_uniqueness_of :ticker, scope: :portfolio_id
-  validates_presence_of :name,
-                        :message => "Stock not found on Yahoo Finance."
-  before_validation :get_quote_name
+  validates_presence_of :ticker, :message => 'Invalid ticker.'
+  validates_presence_of :quantity, :message => 'Must input a number.'
+  validates_presence_of :name, :message => "Search for name revealed no match."
+
+  before_validation :search_name
+  before_create :upcase_ticker
 
   def buy(quantity)
-    ticker.upcase!
-    movements.build(date: DateTime.strptime(quote.last_trade_date, '%m/%d/%Y'), quantity: quantity, price: quote.last_trade_price, trade: 'buy')
+    if get_quote.valid?
+      movements.build(date: DateTime.strptime(get_quote.last_trade_date, '%m/%d/%Y'), quantity: quantity, price: get_quote.last_trade_price, trade: 'buy')
+    end
     save
   end
 
   def sell(quantity)
-    movements.build(date: DateTime.strptime(quote.last_trade_date, '%m/%d/%Y'), quantity: quantity, price: quote.last_trade_price, trade: 'sell')
+    movements.build(date: DateTime.strptime(get_quote.last_trade_date, '%m/%d/%Y'), quantity: quantity, price: get_quote.last_trade_price, trade: 'sell')
     save
   end
 
   private
 
-  def quote
-    @quote ||= CLIENT.quotes([ticker], [:last_trade_price, :last_trade_date, :name, :symbol]).first
+  def get_quote
+    YAHOO_CLIENT.quotes([ticker], [:last_trade_price, :last_trade_date, :name, :symbol]).first
   end
 
-  def get_quote_name
-    self.name = @quote.name
+  def search_name
+    self.name = get_quote['name'] if get_quote['name'] != "N/A"
+  end
+
+  def upcase_ticker
+    ticker.upcase!
   end
 
 end

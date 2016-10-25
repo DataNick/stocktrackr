139f6b8768af5c31e06f8572a5d463c6fbf9a666
DataNick <DataNick@users.noreply.github.com>
2016-10-24 22:16:18 -0400

    Update sell method to include call to update_attribute and self.update_attribute

--- app/models/position.rb
+++ app/models/position.rb
@@ -26,29 +26,41 @@ class Position < ApplicationRecord
   end
 
   def sell(quantity)
-    movements.build(date: DateTime.strptime(get_quote.last_trade_date, '%m/%d/%Y'),
+    @movement = movements.build(date: DateTime.strptime(get_quote.last_trade_date, '%m/%d/%Y'),
                     quantity: quantity,
                     price: get_quote.last_trade_price,
                     trade: 'sell')
+    @movement.update_price_and_date
+    self.update_attribute(:quantity, (self.quantity - quantity))
+    binding.pry
     save
+
+  end
+
+  def position_value
+    (price * quantity).round(2)
+  end
+
+  def update_price
+    update_attribute(:price, get_quote.last_trade_price)
   end
 
   private
 
-  def get_quote
-    YAHOO_CLIENT.quotes([ticker], [:last_trade_price, :last_trade_date, :name, :symbol]).first
-  end
+    def get_quote
+      YAHOO_CLIENT.quotes([ticker], [:last_trade_price, :last_trade_date, :name, :symbol]).first
+    end
 
-  def set_name
-    self.name = get_quote.name if get_quote['name'] != "N/A"
-  end
+    def set_name
+      self.name = get_quote.name if get_quote['name'] != "N/A"
+    end
 
-  def set_price
-    self.price = get_quote.last_trade_price if get_quote['name'] != 'N/A'
-  end
+    def set_price
+      self.price = get_quote.last_trade_price if get_quote['name'] != 'N/A'
+    end
 
-  def upcase_ticker
-    ticker.upcase!
-  end
+    def upcase_ticker
+      ticker.upcase!
+    end
 
 end

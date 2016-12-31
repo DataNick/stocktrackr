762b4844bf1a8468b80a550a40349047ad72f077
DataNick <DataNick@users.noreply.github.com>
2016-12-31 00:05:41 -0500

    Create private methods

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -13,4 +13,16 @@ class Movement < ApplicationRecord
     update_attribute( :date, DateTime.strptime(date.last_trade_date, '%m/%d/%Y'))
   end
 
+  private
+
+    def new_price_data(ticker)
+      price = YAHOO_CLIENT.quotes([ticker], [:last_trade_price]).first
+      price.last_trade_price
+    end
+
+    def new_date_data(ticker)
+      date =  YAHOO_CLIENT.quotes([ticker], [:last_trade_date]).first
+      DateTime.strptime(date.last_trade_date, '%m/%d/%Y')
+    end
+
 end

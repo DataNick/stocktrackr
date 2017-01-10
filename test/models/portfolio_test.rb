b21808e8dfd21d7a8aa6e4533ba9daa34efc830f
DataNick <DataNick@users.noreply.github.com>
2017-01-10 15:46:41 -0500

    Run unit tests on portfolio methods

--- test/models/portfolio_test.rb
+++ test/models/portfolio_test.rb
@@ -9,8 +9,13 @@ class PortfolioTest < ActiveSupport::TestCase
     @position = positions(:one)
   end
 
+  test "should not save without a name being given" do
+    portfolio = Portfolio.new
+    assert_not portfolio.save, " Name must be given."
+  end
+
   test "portfolio value" do
-    assert_equal 6, @portfolio.portfolio_value
+    assert_equal 6.02, @portfolio.portfolio_value.to_f
   end
 
   test "create valuation" do
@@ -19,13 +24,36 @@ class PortfolioTest < ActiveSupport::TestCase
 
   test "valuation amount" do
     @portfolio.portfolio_value #grab values of positions and number of shares for each position
-    assert_equal 6, @portfolio.create_valuation
+    assert_equal 6.02, @portfolio.create_valuation.to_f
   end
 
-  # test "retrieves latest price data" do
-  #   VCR.use_cassette('twitter') do
-  #     @portfolio.new_price_data
-  #   end
-  # end
+  test "calculate amount" do
+    VCR.use_cassette("twitter") do
+      assert_equal 34.90, @portfolio.calculate_amount
+    end
+  end
+
+  test "new price data" do
+    VCR.use_cassette("twitter") do
+      @portfolio.new_price_data
+      assert_equal [34.90], @portfolio.new_price_data
+    end
+  end
+
+  test "update portfolio amount" do
+    VCR.use_cassette("twitter") do
+      @portfolio.update_portfolio_amount
+      assert_equal 34.90, @portfolio.amount.to_f
+    end
+  end
+
+  test "create valuations" do
+    VCR.use_cassette("twitter") do
+      @portfolio.update_portfolio_amount
+    end
+    @portfolio.create_valuation
+    assert_equal 3, Valuation.count #2 valuations are created by default in fixtures
+    assert_equal 34.90, @portfolio.valuations.last.amount.to_f
+  end
 
 end
